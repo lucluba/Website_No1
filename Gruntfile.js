33f1ece60015b07c39b48f6915f53f6c6bf51295
Lukasz Luba <luc.luba@gmail.com>
2017-06-03 19:31:37 +0200

    Gruntfile.js imagemin correction

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
 				files: [{
 					expand: true,
 					cwd: 'images/',
-					src: ['images/*.{png,jpg,gif}'],
+					src: ['./*.{png,jpg,gif}'],
 					dest: 'images/after/'
 				}]
 			}
